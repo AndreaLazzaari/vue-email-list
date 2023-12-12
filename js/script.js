e98416a9e180befad95733a1a5af16c15d8171d9
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-12-12 17:10:44 +0100

    corretta funzione in script.js. Esercizio completato

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ createApp({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail') .then(
                 (response) =>{
                 console.log(response)
-                this.listaEmail.push(response.data);
+                this.listaEmail.push(response.data.response);
     });
             
         }
